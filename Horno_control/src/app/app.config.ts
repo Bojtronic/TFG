f092479c032f9226e50fa064ae6dbc1f9b6f64b4
Bojtronic <bojtronic@gmail.com>
2025-08-09 00:53:01 -0600

    Comunicacion entre Interfaz y ESP32 mediante el broker de EMQX (MQTT y Angular 17)

--- Horno_control/src/app/app.config.ts
+++ Horno_control/src/app/app.config.ts
@@ -1,38 +1,8 @@
-import { ApplicationConfig, InjectionToken } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
-import { MqttService } from 'ngx-mqtt';
-import { IMqttServiceOptions } from 'ngx-mqtt';
 
-// Define el token de inyección que espera ngx-mqtt
-export const MQTT_SERVICE_OPTIONS = new InjectionToken<IMqttServiceOptions>('NgxMqttServiceConfig');
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideAnimations(),
-    provideHttpClient(),
-    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 3000 } },
-    
-    // Configuración MQTT
-    { 
-      provide: MQTT_SERVICE_OPTIONS,
-      useValue: {
-        hostname: 'broker.emqx.io',
-        port: 8084,
-        path: '/mqtt',
-        protocol: 'wss',
-        clientId: 'mqttx_' + Math.random().toString(16).substring(2, 10),
-        clean: true,
-        connectTimeout: 4000,
-        reconnectPeriod: 4000,
-        username: '',
-        password: ''
-      } as IMqttServiceOptions
-    },
-    MqttService
-  ]
+  providers: [provideRouter(routes)]
 };
---
+++ Horno_control/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { IMqttServiceOptions, MqttModule } from 'ngx-mqtt';
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSelectModule} from "@angular/material/select";
+import {FormsModule} from "@angular/forms";
+
+export const connection: IMqttServiceOptions = {
+  hostname: 'broker.emqx.io',
+  //port: 8084,
+  port: 8083,
+  path: '/mqtt',
+  clean: true, // 保留会话
+  connectTimeout: 4000, // 超时时间
+  reconnectPeriod: 4000, // 重连时间间隔
+  // 认证信息
+  //clientId: 'mqttx_b7db442b',
+  clientId: 'mqttx_' + Math.random().toString(16).substring(2, 10),
+  username: '',
+  password: '',
+  //protocol: 'wss',
+  protocol: 'ws',
+  connectOnCreate: false,
+}
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatSelectModule,
+    MqttModule.forRoot(connection)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
